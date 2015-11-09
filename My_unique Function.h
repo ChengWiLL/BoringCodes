5527f385292ea7a6c6bbab4da08625bf16767553
ChengWiLL <1079029559@qq.com>
2015-11-09 15:40:25 +0800

    Create My_unique Function.h

---
+++ My_unique Function.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+	vector<int>::iterator My_unique(vector<int> &nums)
+	{
+		vector<int>::iterator Iter=nums.begin();
+		vector<int>::iterator flag=nums.begin();
+		int tag=0;
+		for(;Iter!=nums.end()-1;++Iter)
+		{
+			if(*Iter==*(Iter+1)&&tag==0)
+			{
+				flag=Iter+1;
+				tag++;
+			}
+			if(*Iter!=*(Iter+1)&&tag!=0)
+			{
+				*flag=*(Iter+1);
+				flag++;
+			}
+		}
+		return flag;
+	}
+};
